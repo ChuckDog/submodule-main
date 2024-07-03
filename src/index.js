44c86391dc5022374bc16d02b1beac7134a05931
ChuckDog <xuzhong2012@my.fit.edu>
2024-07-03 14:50:07 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import test1 from "../submodule-test-1/index.js";
 import test2 from "../submodule-test-2/index.js";
+import test3 from "../submodule-test-3/index.js";
 
-export default { test1, test2 };
+export default { test1, test2, test3 };
